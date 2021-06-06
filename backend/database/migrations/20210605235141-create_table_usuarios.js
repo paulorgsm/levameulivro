2e2f7fd83c59ed66300c87f15af91ccd73dacdab
JG-OLIVEIRA <jorge.gdoliveira*@gmail.com>
2021-06-05 21:02:08 -0300

    criada migrate usuario

---
+++ backend/database/migrations/20210605235141-create_table_usuarios.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.createTable('usuarios', { 
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull: false
+    },
+    senha: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull: false
+    },
+    sobrenome: {
+        type: Sequelize.DataTypes.STRING(100)
+    },
+    cpf: {
+        type: Sequelize.DataTypes.STRING(15)
+    },
+    celular: {
+        type: Sequelize.DataTypes.STRING(15)
+    },
+    data_nasc: {
+        type: Sequelize.DataTypes.DATE
+    },
+    sexo: {
+        type: Sequelize.DataTypes.INTEGER
+    },
+    saldo: {
+        type: Sequelize.DataTypes.STRING(100)
+    }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.dropTable('usuarios');
+  }
+};
