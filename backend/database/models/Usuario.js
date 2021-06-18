b46e18fc41bbd244c639753d5aaf94b0a67ac5bd
JG-OLIVEIRA <jorge.gdoliveira*@gmail.com>
2021-06-18 20:23:29 -0300

    timestamp para true

--- backend/database/models/Usuario.js
+++ backend/database/models/Usuario.js
@@ -1,79 +1,69 @@
 module.exports = (sequelize, DataTypes) => {
-  const Usuario = sequelize.define(
-    "Usuario",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-
-      nome: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-
-      senha: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-
-      email: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-      },
-
-      sobrenome: {
-        type: DataTypes.STRING(100),
-      },
-
-      cpf: {
-        type: DataTypes.STRING(15),
-      },
-
-      celular: {
-        type: DataTypes.STRING(15),
-      },
-
-      data_nasc: {
-        type: DataTypes.DATE,
-      },
-
-      sexo: {
-        type: DataTypes.INTEGER,
-      },
-
-      saldo: {
-        type: DataTypes.STRING(100),
-      },
+  const Usuario = sequelize.define("Usuario", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      timestamp: false,
-    }
-  );
 
-Usuario.associate = function (models) {
+    nome: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+
+    senha: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+
+    sobrenome: {
+      type: DataTypes.STRING(100),
+    },
+
+    cpf: {
+      type: DataTypes.STRING(15),
+    },
+
+    celular: {
+      type: DataTypes.STRING(15),
+    },
+
+    data_nasc: {
+      type: DataTypes.DATE,
+    },
+
+    sexo: {
+      type: DataTypes.INTEGER,
+    },
+
+    saldo: {
+      type: DataTypes.STRING(100),
+    },
+  });
+
+  Usuario.associate = function (models) {
     Usuario.hasMany(models.Livro, {
       foreignKey: "id_usuario",
       as: "livros",
     });
-     Usuario.hasMany(models.Endereco,  {
-      
+    Usuario.hasMany(models.Endereco, {
       foreignKey: "id_usuario",
       as: "enderecos",
-    
     });
-    Usuario.hasMany( models.Pergunta,{
-    
+    Usuario.hasMany(models.Pergunta, {
       foreignKey: "id_usuario",
       as: "perguntas",
-      
     });
     Usuario.hasMany(models.Troca, {
       foreignKey: "id_usuario",
       as: "trocas",
     });
-  }; 
+  };
 
   return Usuario;
 };
