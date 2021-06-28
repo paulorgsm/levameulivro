ba89c0b74a829370488265208cdc20b433fd73b3
JG-OLIVEIRA <jorge.gdoliveira*@gmail.com>
2021-06-27 23:59:25 -0300

    finalizando o crud de todas as tabelas

--- backend/controllers/EntregaController.js
+++ backend/controllers/EntregaController.js
@@ -1,9 +1,44 @@
 const entregaService = require("../services/EntregaService");
 
 const EntregaController = {
-  criar: async (req, res) => {
-    const { rastreio, recebido, id_troca, id_endereco } = req.body;
-    await entregaService(rastreio, recebido, id_troca, id_endereco);
+  create: async (req, res) => {
+    const { rastreio, recebido, id_troca } = req.body;
+
+    return res.json(
+      await entregaService.createEntrega(rastreio, recebido, id_troca)
+    );
+  },
+  update: async (req, res) => {
+    const { rastreio, recebido, id_troca } = req.body;
+
+    const { id } = req.params;
+
+    return res.json(
+      await entregaService.updateEntrega(id, rastreio, recebido, id_troca)
+    );
+  },
+  destroy: async (req, res) => {
+    const { id } = req.params;
+
+    return res.json(await entregaService.destroyEntrega(id));
+  },
+  indexAll: async (req, res) => {
+    return res.json(await entregaService.getAll());
+  },
+  indexById: async (req, res) => {
+    const { id } = req.params;
+
+    return res.json(await entregaService.getById(id));
+  },
+  indexSwapByDeliveryId: async (req, res) => {
+    const { id } = req.params;
+
+    return res.json(await entregaService.getSwapByDeliveryId(id));
+  },
+  indexByIdAndAttribute: async (req, res) => {
+    const { id, attribute } = req.params;
+
+    return res.json(await entregaService.getByIdAndAttribute(id, attribute));
   },
 };
 
--- backend/services/entregaService.js
+++ backend/services/entregaService.js
@@ -1,13 +1,39 @@
 const db = require("../database/models");
 
 const entregaService = {
-  criarEntrega: async (rastreio, recebido, id_troca, id_endereco) => {
-    await db.Entrega.create({
+  createEntrega: async (rastreio, recebido, id_troca) => {
+    return await db.Entrega.create({
       rastreio,
       recebido,
       id_troca,
-      id_endereco,
     });
+  },
+  updateEntrega: async (id, rastreio, recebido, id_troca) => {
+    return await db.Entrega.update(
+      {
+        rastreio,
+        recebido,
+        id_troca,
+      },
+      {
+        where: { id: id },
+      }
+    );
+  },
+  destroyEntrega: async (id) => {
+    return await db.Entrega.destroy({ where: { id: id } });
+  },
+  getAll: async () => {
+    return await db.Entrega.findAll();
+  },
+  getById: async (id) => {
+    return await db.Entrega.findByPk(id);
+  },
+  getSwapByDeliveryId: async (id) => {
+    return await db.Entrega.findByPk(id, { include: "trocas" });
+  },
+  getByIdAndAttribute: async (id, attribute) => {
+    return await db.Entrega.findByPk(id, { attributes: [attribute] });
   },
 };
 
