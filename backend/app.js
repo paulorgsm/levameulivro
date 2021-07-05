c5111d48214db2005548a7bbd693a687e1161d0c
JG-OLIVEIRA <jorge.gdoliveira*@gmail.com>
2021-07-05 11:07:55 -0300

    organizando app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const session = require("express-session");
 const logger = require("morgan");
+const path = require("path");
+const app = express();
 
 const enderecoRouter = require("./routes/endereco");
 const entregaRouter = require("./routes/entrega");
@@ -11,24 +11,9 @@ const perguntaRouter = require("./routes/pergunta");
 const trocaRouter = require("./routes/troca");
 const usuarioRouter = require("./routes/usuario");
 
-const app = express();
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  session({
-    secret: "Leva meu livro",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-/* app.use(cookieParser()); */
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/enderecos", enderecoRouter);
@@ -41,17 +26,6 @@ app.use("/usuarios", usuarioRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
 });
 
 module.exports = app;
