02ce9030b3bac236f7f942f4971fa5b22620ec68
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-07-28 23:57:06 -0300

    resolvendo problema de imagem de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Routes from "./routes";
-import { getName, getPhoto, isAuthenticated, logout } from "./services/auth";
+import { getName, getPhoto} from "./services/auth";
 
 function App() {
-  const oneHour = 6000 * 6000
   const nome = getName()
   const foto = getPhoto()
-  function handleSession() {
-    logout()
-    window.alert("Sessão expirada, por favor logue novamente")
-    window.location.reload();
-  }
-
-  if(isAuthenticated()) {
-    setTimeout(handleSession, oneHour)
-  }
-
+  
   return (
     <BrowserRouter>
-        <Header title={nome ? nome: null} foto={foto ? foto: null}/>
+        <Header title={nome} foto={foto}/>
         <Routes />
         <Footer />
     </BrowserRouter>
--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,14 +1,24 @@
 export const isAuthenticated = () => sessionStorage.getItem('token') !== null;
 export const getToken = () => sessionStorage.getItem('token');
-export const getPhoto = () => sessionStorage.getItem('foto')
 export const getName = () => sessionStorage.getItem('nome');
-export const login = (data) => {
-    sessionStorage.setItem('token', data.token)
-    sessionStorage.setItem('foto', data.foto)
-    sessionStorage.setItem('nome', data.nome)
-};
-export const logout = () => {
+
+export function logout (){
     sessionStorage.removeItem('token')
     sessionStorage.removeItem('foto')
     sessionStorage.removeItem('nome')
 };
+
+export function login(data){
+    sessionStorage.setItem('token', data.token)
+    sessionStorage.setItem('foto', data.foto)
+    sessionStorage.setItem('nome', data.nome)
+};
+
+export function getPhoto(){
+    const foto = sessionStorage.getItem('foto')
+    if (foto == "undefined") {
+        return null
+    } else {
+        return foto
+    }
+}
