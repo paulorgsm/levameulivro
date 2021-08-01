64dee1d87e22c72a71cfba607d8548235c00670d
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2021-08-01 03:24:50 -0300

    interceptando erro 401 e fazendo logout

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,17 @@
 import axios from "axios";
+import { logout } from "./auth";
 
 const api = axios.create({ baseURL: 'http://localhost:3000' })
+
+api.interceptors.response.use(
+    function (response) {
+        return response
+    }, 
+    function (error) {
+        if (error.response.status == 401) {
+            logout()
+        }
+    }
+    );
             
 export default api;
