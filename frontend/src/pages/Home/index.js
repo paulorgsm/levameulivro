12cf949c5fc96821bfc00c97b020e688add32469
drull1000 <pcaetanop@protonmail.com>
2021-09-07 12:46:24 -0300

    Fixing console errors & warnings

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -29,7 +29,7 @@ function Home() {
     <StyledHome>
       <section className="carousel" aria-label="Gallery">
         <ol className="carousel__viewport">
-          <li id="carousel__slide1" tabindex="0" className="carousel__slide">
+          <li id="carousel__slide1" tabIndex="0" className="carousel__slide">
             <div className="carousel__snapper">
               <a href="#carousel__slide4" className="carousel__prev">
                 Go to last slide
@@ -40,7 +40,7 @@ function Home() {
               </a>
             </div>
           </li>
-          <li id="carousel__slide2" tabindex="0" className="carousel__slide">
+          <li id="carousel__slide2" tabIndex="0" className="carousel__slide">
             <div className="carousel__snapper"></div>
             <a href="#carousel__slide1" className="carousel__prev">
               Go to previous slide
@@ -50,7 +50,7 @@ function Home() {
               Go to next slide
             </a>
           </li>
-          <li id="carousel__slide3" tabindex="0" className="carousel__slide">
+          <li id="carousel__slide3" tabIndex="0" className="carousel__slide">
             <div className="carousel__snapper"></div>
             <a href="#carousel__slide2" className="carousel__prev">
               Go to previous slide
@@ -60,7 +60,7 @@ function Home() {
               Go to next slide
             </a>
           </li>
-          <li id="carousel__slide4" tabindex="0" className="carousel__slide">
+          <li id="carousel__slide4" tabIndex="0" className="carousel__slide">
             <div className="carousel__snapper"></div>
             <a href="#carousel__slide3" className="carousel__prev">
               Go to previous slide
@@ -194,7 +194,7 @@ function Home() {
 
           <div className="btn">
             <input className="btn1" type="button" value="QUERO ESTE!" />
-            <input class="btn2" type="button" value="+DETALHES" />
+            <input className="btn2" type="button" value="+DETALHES" />
           </div>
 
           <div className="img-assets">
@@ -328,7 +328,7 @@ function Home() {
           <div className="vectorUl">
             <ul className="linkul">
               <li>
-                <a class="link" href="#">
+                <a className="link" href="#">
                   LEIA MAIS
                 </a>
               </li>
